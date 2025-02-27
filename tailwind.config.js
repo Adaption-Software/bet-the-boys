2454fb29c0356d0e02bb29648f6b9a2d3a81a5f7
Shane <shanepurgason.98@gmail.com>
2025-02-26 23:15:59 -0500

    Implement primary theme color
    - Added Form.vue and TextField.vue
    
    - Updated colors on GuestLayout.vue
    
    - Used new form and input field component in Login.vue

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,21 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                "primary": {
+                    50: "#E8E8E8",
+                    100: "#CFCFCF",
+                    200: "#9E9E9E",
+                    300: "#6E6E6E",
+                    400: "#3D3D3D",
+                    500: "#0E0E0E",
+                    600: "#0A0A0A",
+                    700: "#080808",
+                    800: "#050505",
+                    900: "#030303",
+                    950: "#030303"
+                }
+            }
         },
     },
 
