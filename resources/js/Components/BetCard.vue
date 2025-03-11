72c0e96233012407b295456fa2d52b5b56a2f636
Tyler Walton <tylerwalton300@gmail.com>
2025-03-10 21:44:40 -0400

    creating bet card

---
+++ resources/js/Components/BetCard.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({
+    image: {
+        type: String,
+        required: true
+    },
+    teamName: {
+        type: String,
+        required: true,
+    },
+    eventDate: {
+        type: Object,
+        required: true,
+    },
+    location: {
+        type: String,
+        required: true
+    }
+
+})
+</script>
+
+<template>
+
+</template>
