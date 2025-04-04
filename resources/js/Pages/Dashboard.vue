2574eab67a718f6c34f171b92742895b1dbd699a
Shane <shanepurgason.98@gmail.com>
2025-04-03 22:36:02 -0400

    Dashboard rework (wip)

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
-
+import StatCard from "@/Components/Stat/StatCard.vue";
 </script>
 
 <template>
@@ -16,12 +16,22 @@ import { Head } from '@inertiajs/vue3';
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden shadow-sm sm:rounded-lg bg-secondary-500"
-                >
-                    <div class="p-6 text-gray-100">
-                        You're logged in!
-                    </div>
+                <div class="overflow-hidden shadow-sm sm:rounded-lg bg-secondary-500 grid grid-cols-3 p-3 gap-3 text-gray-200">
+                    <StatCard
+                        title="Won"
+                        stat="7"
+                        trend="5"
+                    ></StatCard>
+
+                    <StatCard
+                        title="Lost"
+                        stat="7"
+                    ></StatCard>
+
+                    <StatCard
+                        title="Placed"
+                        stat="7"
+                    ></StatCard>
                 </div>
             </div>
         </div>
