c741952b7a5bd5ca29909ff6c1bd59a943c85fea
Shane <shanepurgason.98@gmail.com>
2025-05-12 22:22:14 -0400

    Show NFL bets and placing bets (wip)

---
+++ resources/js/Pages/Football/Index.vue
@@ -0,0 +1,43 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import { useBets } from '@/scripts/stores/bets.js';
+import { storeToRefs } from 'pinia';
+import { onMounted, onUnmounted } from 'vue';
+import BetCard from '@/Components/Bet/BetCard.vue';
+
+const store = useBets();
+const { allBets } = storeToRefs(useBets());
+
+onMounted(() => {
+    store.getBets('football');
+});
+
+onUnmounted(() => {
+    store.$reset();
+});
+</script>
+
+<template>
+    <Head title="Football" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-xl font-semibold leading-tight text-gray-200">
+                NFL
+            </h2>
+        </template>
+
+        <div class="py-12 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="grid sm:grid-cols-2 gap-8 p-4 md:w-full">
+                <BetCard
+                    v-for="(bet, key) in allBets"
+                    :key="key"
+                    :event="key"
+                    icon="/images/nfl-logo.png"
+                    v-bind="bet"
+                />
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
