34f0b095ad57f10aedab77b44c3407bd7e5ae06b
Tyler Walton <tylerwalton300@gmail.com>
2025-05-26 21:35:51 -0400

    better prepared data for dt

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Bet;
 use Illuminate\Support\Carbon;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Collection;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -13,21 +13,25 @@ class DashboardController extends Controller
     public function __invoke(): Response
     {
         $bets = Bet::query()
-            ->where('user_id', Auth::id())
-            ->whereBetween('created_at', [Carbon::now()->subMonth(), Carbon::now()])
+            ->leftJoin('users', 'users.id', '=', 'bets.user_id')
+            ->whereBetween('bets.created_at', [Carbon::now()->subMonth(), Carbon::now()])
             ->get()
             ->map(function (Bet $bet) {
                 return [
+                    'user' => $bet->name,
                     'bet_placed_at' => $bet->created_at->format('m/d/Y g:ia'),
                     'outcome' => $bet->outcome->label(),
                     'sport' => $bet->sport->label(),
                     'bet_type' => $bet->bet_type->label(),
                 ];
-            });
+            })
+            ->groupBy('user');
 
-        $wins = $bets->where('outcome', 'Win')->count();
-        $losses = $bets->where('outcome', 'Lose')->count();
-        $placed = $bets->count();
+        $wins = $bets->sum(fn (Collection $bet) => $bet->where('outcome', 'Win')->count());
+
+        $losses = $bets->sum(fn (Collection $bet) => $bet->where('outcome', 'Lose')->count());
+
+        $placed = $bets->sum(fn (Collection $bet) => $bet->count());
 
         return Inertia::render('Dashboard')
             ->with('bets', $bets)
